a67d96e16fb989dbdde37271ace8a75be13f4f76
marco serinelli <silsniper@gmail.com>
2022-04-01 11:24:07 +0200

    Completamento della applicazione spring

---
+++ applicazionerestspring/src/test/java/it/devlec/applicazionerestspring/ApplicazionerestspringApplicationTests.java
@@ -0,0 +1,13 @@
+package it.devlec.applicazionerestspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicazionerestspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
