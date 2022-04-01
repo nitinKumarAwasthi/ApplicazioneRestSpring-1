1b2eaf2e0e67ea49456fd76a65a26b3a0d5740ed
marco serinelli <silsniper@gmail.com>
2022-04-01 12:40:50 +0200

    Aggiunta del mio primo bean e l'utlizzo di una chiave nel file properties

--- applicazionerestspring/src/main/java/it/devlec/applicazionerestspring/ApplicazionerestspringApplication.java
+++ applicazionerestspring/src/main/java/it/devlec/applicazionerestspring/ApplicazionerestspringApplication.java
@@ -1,13 +1,46 @@
 package it.devlec.applicazionerestspring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ApplicazionerestspringApplication {
-
+	//creazione attributo log
+	private static final Logger logger = LoggerFactory
+			.getLogger(ApplicazionerestspringApplication.class);
+	//utilizzo di una chiave delle proprietà di Spring
+	@Value("${it.devlec.applicazionerestspring.saluti}")
+	public String salutoIniziale;
+	//MAIN inizia qui
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicazionerestspringApplication.class, args);
+	}
+	//il nostro primo bean
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+		return args -> {
+			logger.debug(salutoIniziale);
+			String[] beans = ctx.getBeanDefinitionNames();
+			Arrays.sort(beans);
+			for(String bean : beans){
+				logger.debug(bean);
+			}
+			for(int i=0; i< beans.length; i++){
+				logger.warn(beans[i]);
+			}
+			for(int i=0; i< 10; i++){
+				logger.error(beans[i]);
+			}
+		};
+
 	}
 
 }
