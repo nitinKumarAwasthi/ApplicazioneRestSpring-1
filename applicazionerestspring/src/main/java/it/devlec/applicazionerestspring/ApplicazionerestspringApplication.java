50327e9db65d3e37b7b22894a8ad7fb6b3d3318f
marco serinelli <silsniper@gmail.com>
2022-04-01 11:19:44 +0200

    Aggiunta del main spring

---
+++ applicazionerestspring/src/main/java/it/devlec/applicazionerestspring/ApplicazionerestspringApplication.java
@@ -0,0 +1,13 @@
+package it.devlec.applicazionerestspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicazionerestspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicazionerestspringApplication.class, args);
+	}
+
+}
